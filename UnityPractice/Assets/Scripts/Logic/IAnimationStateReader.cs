edc33e9b80cb5620e48dbdfc34b2543e03d0cf3c
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-02-01 17:38:29 +0500

    feat(Animation): implemented animator scripts

---
+++ UnityPractice/Assets/Scripts/Logic/IAnimationStateReader.cs
@@ -0,0 +1,9 @@
+namespace UnityPractice.Logic
+{
+    public interface IAnimationStateReader
+    {
+        void EnteredState(int stateHash);
+        void ExitedState(int stateHash);
+        AnimatorState State { get; }
+    }
+}
