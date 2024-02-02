a88e050e7063784e68d8d387227e1302a4f9c78b
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-02-02 17:56:26 +0500

    feat(assets): implemented assets provider

---
+++ UnityPractice/Assets/Scripts/Infrastructure/AssetManagement/IAssets.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityPractice.Infrastructure.Services;
+
+namespace UnityPractice.Infrastructure.AssetManagement
+{
+    public interface IAssets : IService
+    {
+        GameObject Instantiate(string path, Vector3 at);
+        GameObject Instantiate(string path);
+    }
+}
