aaf761f69543ec2a8b0f204ce3c753ccbe2a61fa
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-02-05 13:21:46 +0500

    feat(Data): implemented LoadProgressState

---
+++ UnityPractice/Assets/Scripts/Infrastructure/States/LoadProgressState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Data;
+using UnityPractice.Infrastructure.Services.PersistentProgress;
+using UnityPractice.Infrastructure.Services.SaveLoad;
+
+namespace UnityPractice.Infrastructure.States
+{
+    public class LoadProgressState : IState
+    {
+        private readonly GameStateMachine _stateMachine;
+        private readonly IPersistentProgressService _progressService;
+        private readonly ISaveLoadService _saveLoadService;
+
+        public LoadProgressState(GameStateMachine stateMachine, IPersistentProgressService progressService, ISaveLoadService saveLoadService)
+        {
+            _stateMachine = stateMachine;
+            _progressService = progressService;
+            _saveLoadService = saveLoadService;
+        }
+
+        public void Enter()
+        {
+            LoadProgressOrInitNew();
+            
+            _stateMachine.EnterIn<LoadLevelState, string>(_progressService.PlayerProgress.WorldData.PositionOnLevel.Level);
+        }
+
+        public void Exit()
+        {
+            
+        }
+
+        private void LoadProgressOrInitNew()
+        {
+            _progressService.PlayerProgress = _saveLoadService.LoadProgress() ?? NewProgress();
+        }
+
+        private PlayerProgress NewProgress() => new(initialLevel: "Main");
+    }
+}
