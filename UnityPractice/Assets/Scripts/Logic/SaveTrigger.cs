0dc1d9579ffe6be8780ff882a0b337a440819afc
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-02-05 13:24:29 +0500

    feat(Data): implemented saveTrigger

---
+++ UnityPractice/Assets/Scripts/Logic/SaveTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityPractice.Character;
+using UnityPractice.Infrastructure.Services;
+using UnityPractice.Infrastructure.Services.SaveLoad;
+
+namespace UnityPractice.Logic
+{
+    public class SaveTrigger : MonoBehaviour
+    {
+        [SerializeField] private BoxCollider boxCollider;
+        
+        private ISaveLoadService _saveLoadService;
+        
+        private void Awake()
+        {
+            _saveLoadService = AllServices.Container.Single<ISaveLoadService>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out ISavedProgress progressWriter))
+            {
+                _saveLoadService.SaveProgress();
+                Debug.Log("Progress saved");
+                gameObject.SetActive(false);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (boxCollider == null)
+                return;
+            
+            Gizmos.color = new Color32(30, 200, 30, 130);
+            Gizmos.DrawCube(transform.position, boxCollider.size);
+            Gizmos.color = Color.white;
+        }
+    }
+}
