f40af0f07f221812e948f21363bee45d81ab7975
Sergei Shalamow <86056583+Indexfue@users.noreply.github.com>
2024-02-05 13:08:41 +0500

    feat(Data): added data reader/writer to factory

--- UnityPractice/Assets/Scripts/Infrastructure/Factories/IGameFactory.cs
+++ UnityPractice/Assets/Scripts/Infrastructure/Factories/IGameFactory.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityPractice.Character;
 using UnityPractice.Infrastructure.Services;
 
 namespace UnityPractice.Infrastructure.Factories
 {
     public interface IGameFactory : IService
     {
+        List<ISavedProgressReader> ProgressReaders { get; }
+        List<ISavedProgress> ProgressWriters { get; }
+        
         GameObject CreateHero(GameObject at);
         GameObject CreateHud();
+        void CleanUp();
     }
 }
