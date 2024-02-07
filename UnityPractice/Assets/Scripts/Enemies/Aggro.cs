ea6e8210b3cc79d48240c9983731a5bb53372555
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-07 22:09:38 +0500

    feat(Enemy): enemy aggro state

---
+++ UnityPractice/Assets/Scripts/Enemies/Aggro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityPractice.Enemies
+{
+    public class Aggro : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver triggerObserver;
+        [SerializeField] private AgentMoveToPlayer follow;
+        
+        private void Start()
+        {
+            triggerObserver.TriggerEntered += OnTriggerEntered;
+            triggerObserver.TriggerExited += OnTriggerExited;
+
+            follow.enabled = false;
+        }
+
+        private void OnTriggerEntered(Collider other) => 
+            follow.enabled = true;
+
+        private void OnTriggerExited(Collider other) => 
+            follow.enabled = false;
+    }
+}
