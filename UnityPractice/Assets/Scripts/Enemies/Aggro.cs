9c9fcc057196bb56847aadcf24fa7e85f23ece5e
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-07 22:42:50 +0500

    feat(Enemy):  added aggro time

--- UnityPractice/Assets/Scripts/Enemies/Aggro.cs
+++ UnityPractice/Assets/Scripts/Enemies/Aggro.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace UnityPractice.Enemies
@@ -6,8 +7,12 @@ namespace UnityPractice.Enemies
     public class Aggro : MonoBehaviour
     {
         [SerializeField] private TriggerObserver triggerObserver;
-        [SerializeField] private AgentMoveToPlayer follow;
-        
+        [SerializeField] private Follow follow;
+        [SerializeField, Min(0.1f)] private float aggroTime;
+
+        private Coroutine _aggroCoroutine;
+        private bool _hasAggroObject;
+
         private void Start()
         {
             triggerObserver.TriggerEntered += OnTriggerEntered;
@@ -16,10 +21,38 @@ namespace UnityPractice.Enemies
             follow.enabled = false;
         }
 
-        private void OnTriggerEntered(Collider other) => 
-            follow.enabled = true;
+        private void OnTriggerEntered(Collider other)
+        {
+            if (!_hasAggroObject)
+            {
+                _hasAggroObject = true;
+                follow.enabled = true;
+                StopAggroCoroutine();
+            }
+        }
 
-        private void OnTriggerExited(Collider other) => 
+        private void OnTriggerExited(Collider other)
+        {
+            if (_hasAggroObject)
+            {
+                _hasAggroObject = false;
+                _aggroCoroutine = StartCoroutine(DisableFollowIfAggroTimeExpired());
+            }
+        }
+
+        private IEnumerator DisableFollowIfAggroTimeExpired()
+        {
+            yield return new WaitForSeconds(aggroTime);
             follow.enabled = false;
+        }
+
+        private void StopAggroCoroutine()
+        {
+            if (_aggroCoroutine != null)
+            {
+                StopCoroutine(_aggroCoroutine);
+                _aggroCoroutine = null;
+            }
+        }
     }
 }
